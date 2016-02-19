6b9890b36e4dbfe9db637203a4ded0e7a05c133d
hoiyd <hoiyd@hotmail.com>
2016-02-19 12:40:00 +0800

    ex1.6 is accidentally overriden, bring it back

--- ch1/ex1.6/lissajous1_6.go
+++ ch1/ex1.6/lissajous1_6.go
@@ -10,7 +10,7 @@ import (
 	"os"
 )
 
-var palette = []color.Color{color.Black, color.RGBA{0x00, 0xFF, 0x00, 0xFF}}
+var palette = []color.Color{color.White, color.RGBA{0x00, 0xFF, 0x00, 0xFF}, color.RGBA{0xFF, 0x00, 0x00, 0xFF}, color.RGBA{0x00, 0x00, 0xFF, 0xFF}}
 
 const (
 	whiteIndex = 0
@@ -38,7 +38,7 @@ func lissajous(out io.Writer) {
 		for t := 0.0; t < cycles*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), uint8(rand.Int()%4))
 		}
 		phase += 0.1
 		anim.Delay = append(anim.Delay, delay)
