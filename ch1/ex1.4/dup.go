c411f5e974015a99c7b3c244f922cff46f0a5f31
hoiyd <zxue@freewheel.tv>
2016-02-19 12:31:02 +0800

    Add ex1.4

---
+++ ch1/ex1.4/dup.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines("", os.Stdin, counts)
+	} else {
+		for _, file := range files {
+			f, err := os.Open(file)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(file, f, counts)
+			f.Close()
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+
+func countLines(file string, f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		if file != "" {
+			counts[file+":"+input.Text()]++
+		} else {
+			counts[input.Text()]++
+		}
+	}
+}
