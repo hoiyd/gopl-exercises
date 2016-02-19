6698eb50ee012f01ed7c7476ae6716ccc5580c62
hoiyd <hoiyd@hotmail.com>
2016-02-19 13:20:35 +0800

    Add ex1.8

---
+++ ch1/ex1.8/fetch1_8.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+const (
+	prefix = "http://"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, prefix) {
+			url = prefix + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		// The first return value of io.Copy is the nuber of bytes that are copied,
+		// which in this case, is useless
+		_, err = io.Copy(os.Stdout, resp.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
