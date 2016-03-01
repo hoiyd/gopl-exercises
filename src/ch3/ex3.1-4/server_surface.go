23e314ff2a49752e80dd9a7df82ac7320e4cddd8
hoiyd <hoiyd@hotmail.com>
2016-03-01 14:52:38 +0800

    format code

--- src/ch3/ex3.1-4/server_surface.go
+++ src/ch3/ex3.1-4/server_surface.go
@@ -21,10 +21,12 @@ var sin30, cos30 = math.Sin(angle), math.Cos(angle)
 
 func surface(out io.Writer) {
 	var s string
+
 	s = fmt.Sprintf("<svg xmlns='http://www.w3.org/2000/svg' "+
 		"style='stroke: grey;fill: white;stroke-width: 0.7' "+
 		"width='%d' height='%d'>", width, height)
 	out.Write([]byte(s))
+
 	for i := 0; i < cells; i++ {
 		for j := 0; j < cells; j++ {
 			ax, ay := corner(i+1, j)
@@ -68,28 +70,3 @@ func main() {
 	})
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
-
-// func lissajous(out io.Writer, cycles int) {
-// 	const (
-// 		res     = 0.01
-// 		size    = 100
-// 		nframes = 128
-// 		delay   = 8
-// 	)
-// 	freq := rand.Float64() * 3.0
-// 	anim := gif.GIF{LoopCount: nframes}
-// 	phase := 0.0
-// 	for i := 0; i < nframes; i++ {
-// 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
-// 		img := image.NewPaletted(rect, palette)
-// 		for t := 0.0; t < float64(cycles)*2*math.Pi; t += res {
-// 			x := math.Sin(t)
-// 			y := math.Sin(t*freq + phase)
-// 			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), uint8(rand.Int()%4))
-// 		}
-// 		phase += 0.1
-// 		anim.Delay = append(anim.Delay, delay)
-// 		anim.Image = append(anim.Image, img)
-// 	}
-// 	gif.EncodeAll(out, &anim)
-// }
