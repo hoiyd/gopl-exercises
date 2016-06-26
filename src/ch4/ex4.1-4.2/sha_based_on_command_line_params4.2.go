c443138e18f7105bb0dd495a3478b8e4a7f9054f
zxue <hoiyd@hotmail.com>
2016-06-26 16:01:26 +0800

    Add ex4.2

---
+++ src/ch4/ex4.1-4.2/sha_based_on_command_line_params4.2.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"crypto/sha256"
+	"crypto/sha512"
+	"flag"
+	"fmt"
+	"strings"
+)
+
+func main() {
+	sha := flag.String("sha", "256", "Set the length of hash. Can be set to 256, 384 or 512")
+	flag.Parse()
+
+	content := strings.Join(flag.Args(), "")
+	fmt.Println(content)
+
+	bytes := []byte(content)
+
+	switch *sha {
+	case "256":
+		fmt.Printf("%x\n", sha256.Sum256(bytes))
+	case "384":
+		fmt.Printf("%x\n", sha512.Sum384(bytes))
+	case "512":
+		fmt.Printf("%x\n", sha512.Sum512(bytes))
+	default:
+		fmt.Printf("%s is not a valid option: 256 | 383 | 512\n", *sha)
+	}
+}
