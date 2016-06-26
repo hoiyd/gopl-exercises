5d1220dd5ced5790b98f72ae2a5c61020ef174f1
zxue <hoiyd@hotmail.com>
2016-06-26 14:00:33 +0800

    remove type of consts since they can be untyped

--- src/ch3/const3.13.go
+++ src/ch3/const3.13.go
@@ -3,14 +3,14 @@ package main
 import "fmt"
 
 const (
-	KB uint64 = 1000
-	MB        = KB * KB
-	GB        = MB * KB
-	TB        = GB * KB
-	PB        = TB * KB
-	EB        = PB * KB
-	ZB        = EB * KB //exceeds uint64
-	YB        = ZB * KB
+	KB = 1000
+	MB = KB * KB
+	GB = MB * KB
+	TB = GB * KB
+	PB = TB * KB
+	EB = PB * KB
+	ZB = EB * KB //exceeds uint64
+	YB = ZB * KB
 )
 
 func main() {
