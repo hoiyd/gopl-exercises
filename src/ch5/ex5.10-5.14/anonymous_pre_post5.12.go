c78702cc5bd4d5155de5e53b2fddda67b6a32b73
zxue <hoiyd@hotmail.com>
2016-08-11 21:36:00 +0800

    Add ex5.12, make 'pre' and 'post' function anonymous

---
+++ src/ch5/ex5.10-5.14/anonymous_pre_post5.12.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/html"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		err := fetchHTMLContent(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch html content: %v\n", err)
+			continue
+		}
+	}
+}
+
+func fetchHTMLContent(url string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return fmt.Errorf("getting %s: %s", url, resp.Status)
+	}
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return fmt.Errorf("parsing %s as HTML: %v", url, err)
+	}
+
+	var depth int
+	startElement := func(n *html.Node) {
+		if n.Type == html.ElementNode {
+			fmt.Printf("%*s<%s>\n", depth*2, "", n.Data)
+			depth++
+		}
+	}
+
+	endElement := func(n *html.Node) {
+		if n.Type == html.ElementNode {
+			depth--
+			fmt.Printf("%*s</%s>\n", depth*2, "", n.Data)
+		}
+	}
+
+	forEachDoc(doc, startElement, endElement)
+	return nil
+}
+
+func forEachDoc(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachDoc(c, pre, post)
+	}
+
+	if post != nil {
+		post(n)
+	}
+}
